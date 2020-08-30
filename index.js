b518696100327097db0994991d9d494d25d40d37
AiswaryaBalu-5506 <aischandar@gmail.com>
2020-08-31 00:06:56 +0530

    akes-and-icecreams

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const redux = require('redux')
+const combineReducers = redux.combineReducers
 const createStore = redux.createStore
 const BUY_CAKE = 'BUY_CAKE'
 const BAKE_CAKE = 'BAKE_CAKE'
+const BUY_ICECREAM = 'BUY_ICECREAM'
 
 //ACTION
 function buyCake(){
@@ -18,13 +20,23 @@ function bakeCake(){
     }
 }
 
+function buyIceCream(){
+    return {
+        type: BUY_ICECREAM,
+        info: 'Cuatomer buys ice cream'
+    }
+}
+
 //STATE
-const initialState = {
+const initialCakeState = {
     numOfCakes: 10
+}
+const initialIceCreamState = {
+    numOfIceCreams: 20
 }
 
 //REDUCER (prevState, action) => newState
-const reducer = (state = initialState, action) => {
+const reducerCake = (state = initialCakeState, action) => {
     switch(action.type){
         case BUY_CAKE: return{
             ...state, //always create a copy of the state object, and chage only the required elements
@@ -38,7 +50,22 @@ const reducer = (state = initialState, action) => {
     }
 }
 
-const store = createStore(reducer) //The reducer has the initial state - responsibility 1
+const reducerIceCream = (state = initialIceCreamState, action) => {
+    switch(action.type){
+        case BUY_ICECREAM: return{
+            ...state, //always create a copy of the state object, and chage only the required elements
+            numOfIceCreams: state.numOfIceCreams- 1
+        }
+        
+        default: return state
+    }
+}
+
+const rootReducer = combineReducers({
+    cake: reducerCake,
+    icecream: reducerIceCream
+})
+const store = createStore(rootReducer) //The reducer has the initial state - responsibility 1
 console.log(store.getState())  //Allows the access of the current state via getState() - responsibility 2
 //Responsibility 4 - should allow apps to subscribe to the redux store
 const unsubscribe = store.subscribe( () => {
@@ -47,6 +74,8 @@ const unsubscribe = store.subscribe( () => {
 store.dispatch(buyCake()) //responsibility 3 - the store allows dispatchers to alter the state, i.e., allows an action to occur.
 store.dispatch(bakeCake())
 store.dispatch(buyCake())
+store.dispatch(buyIceCream())
+store.dispatch(buyIceCream())
 //unsubscribing - resonsibility 5
 unsubscribe()
 
